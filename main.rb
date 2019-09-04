39e751275f6dd08aa1cb55ae0dc8be2bc5673a60
vxd7 <ykonpaku13@gmail.com>
2019-09-04 21:06:05 +0300

    Added Route class

--- main.rb
+++ main.rb
@@ -19,6 +19,30 @@ class Station
 end
 
 class Route
+  def initialize(start_station, end_station)
+    @start_station = start_station
+    @end_station = end_station
+
+    @intermediate_stations = []
+  end
+
+  def add_intermediate_station(station)
+    @intermediate_stations << station
+  end
+
+  def remove_intermediate_station(station)
+    if @intermediate_stations.include? station
+      @intermediate_stations.delete station
+    end
+  end
+
+  def list_all_stations
+    all_stations = [@start_station] + @intermediate_stations + [@end_station]
+    all_stations.each do |station|
+      puts station
+    end
+  end
+
 end
 
 class Train
