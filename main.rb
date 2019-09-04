7e1171cf91501b1a11abc4dbd82102ef5ae03dc5
vxd7 <ykonpaku13@gmail.com>
2019-09-04 22:29:34 +0300

    Added some testing

--- main.rb
+++ main.rb
@@ -119,3 +119,35 @@ class Train
     @route.all_stations[(@cur_station_index + 1) % @route_length]
   end
 end
+
+station1 = Station.new("station1")
+station2 = Station.new("station2")
+station3 = Station.new("station3")
+station4 = Station.new("station4")
+station5 = Station.new("station5")
+
+# Make route st1 -> st2 -> st3 -> st4 -> st5
+route = Route.new(station1, station5)
+route.add_intermediate_station(station2)
+route.add_intermediate_station(station3)
+route.add_intermediate_station(station4)
+
+# Make train
+train = Train.new(123, "passenger", 10)
+train.set_route(route)
+puts train.get_cur_station.name
+
+train.traverse_next_station
+puts train.get_cur_station.name
+
+train.traverse_next_station
+puts train.get_cur_station.name
+ 
+train.traverse_next_station
+puts train.get_cur_station.name
+
+train.traverse_next_station
+puts train.get_cur_station.name
+
+train.traverse_next_station
+puts train.get_cur_station.name
