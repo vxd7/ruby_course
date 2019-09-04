ced5e5e2987e2a25b1ffe133d85d74880d25f60e
vxd7 <ykonpaku13@gmail.com>
2019-09-04 21:50:36 +0300

    Modified Route class to always have full route in instance variable

--- main.rb
+++ main.rb
@@ -19,30 +19,33 @@ class Station
 end
 
 class Route
+  attr_reader :all_stations
+
   def initialize(start_station, end_station)
     @start_station = start_station
     @end_station = end_station
 
     @intermediate_stations = []
+    @all_stations = [@start_station, @end_station]
   end
 
   def add_intermediate_station(station)
     @intermediate_stations << station
+    @all_stations = [@start_station] + @intermediate_stations + [@end_station]
   end
 
   def remove_intermediate_station(station)
     if @intermediate_stations.include? station
       @intermediate_stations.delete station
+      @all_stations = [@start_station] + @intermediate_stations + [@end_station]
     end
   end
 
   def list_all_stations
-    all_stations = [@start_station] + @intermediate_stations + [@end_station]
-    all_stations.each do |station|
+    self.all_stations.each do |station|
       puts station
     end
   end
-
 end
 
 class Train
