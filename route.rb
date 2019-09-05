73b3f09a2d33d109e522e0fd0b4af9d3ee69225f
vxd7 <ykonpaku13@gmail.com>
2019-09-05 20:35:03 +0300

    all_stations -> station name change

--- route.rb
+++ route.rb
@@ -2,18 +2,18 @@ class Route
   attr_reader :all_stations
 
   def initialize(start_station, end_station)
-    @all_stations = [start_station, end_station]
+    @stations = [start_station, end_station]
   end
 
   def add_intermediate_station(station)
-    @all_stations.insert(-2, station)
+    @stations.insert(-2, station)
   end
 
   def remove_intermediate_station(station)
-    @all_stations.delete(station)
+    @stations.delete(station)
   end
 
   def list_all_stations
-    @all_stations.each { |station| puts station }
+    @stations.each { |station| puts station }
   end
 end
