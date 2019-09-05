c826d25ef35488dda77baa2dd6d209f896fc3f2c
vxd7 <ykonpaku13@gmail.com>
2019-09-05 20:51:01 +0300

    Test remove_intermediate_station

--- railroads_test.rb
+++ railroads_test.rb
@@ -37,3 +37,22 @@ puts "There are cargo trains station 1" if not station1.trains_by_type("cargo").
 
 puts "There are passenger trains on station5" if not station5.trains_by_type("passenger").empty?
 puts "There are cargo trains station 5" if not station5.trains_by_type("cargo").empty?
+
+# Test remove_intermediate_stations
+# This should not remove station1
+puts "After remove_intermediate_station(station1)"
+route.remove_intermediate_station(station1)
+route.list_all_stations
+
+# This should not remove station5
+puts "After remove_intermediate_station(station5)"
+route.remove_intermediate_station(station5)
+route.list_all_stations
+
+route.remove_intermediate_station(station3)
+route.remove_intermediate_station(station2)
+route.remove_intermediate_station(station4)
+
+# There should be now only two stations left -- first and last
+puts "After removing every intermediate station"
+route.list_all_stations
