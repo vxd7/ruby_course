342532c0c1199054cc272fcaafcf2582db171851
vxd7 <ykonpaku13@gmail.com>
2019-09-05 14:35:05 +0300

    Changed the way the route is constructed

--- route.rb
+++ route.rb
@@ -2,28 +2,18 @@ class Route
   attr_reader :all_stations
 
   def initialize(start_station, end_station)
-    @start_station = start_station
-    @end_station = end_station
-
-    @intermediate_stations = []
-    @all_stations = [@start_station, @end_station]
+    @all_stations = [start_station, end_station]
   end
 
   def add_intermediate_station(station)
-    @intermediate_stations << station
-    @all_stations = [@start_station] + @intermediate_stations + [@end_station]
+    @all_stations.insert(@all_stations.length - 1, station)
   end
 
   def remove_intermediate_station(station)
-    if @intermediate_stations.include? station
-      @intermediate_stations.delete station
-      @all_stations = [@start_station] + @intermediate_stations + [@end_station]
-    end
+    @all_stations.delete(station)
   end
 
   def list_all_stations
-    self.all_stations.each do |station|
-      puts station
-    end
+    self.all_stations.each { |station| puts station }
   end
 end
