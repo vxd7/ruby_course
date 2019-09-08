c2919e4a13cab0eb3cc1482757e7668b543c6a3d
vxd7 <ykonpaku13@gmail.com>
2019-09-08 19:37:30 +0300

    Added requires

--- cargo_train.rb
+++ cargo_train.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'train'
+
 class CargoTrain < Train
   def add_carriage(carriage)
     return unless carirage.type == @type
--- passenger_train.rb
+++ passenger_train.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'train'
+
 class PassengerTrain < Train
   def add_carriage(carriage)
     return unless carriage.type == @type
