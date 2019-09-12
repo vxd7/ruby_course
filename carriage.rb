23fd54c24dc6722a6feb775f9b7bf81c25ab7ee9
vxd7 <ykonpaku13@gmail.com>
2019-09-12 20:42:22 +0300

    Added id attribute to carriage

--- carriage.rb
+++ carriage.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Carriage
-  attr_reader :type
+  attr_reader :type, :id
 
-  def initialize(type)
+  def initialize(type, id)
     @type = type
+    @id = id
   end
 end
