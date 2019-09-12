4f9b3e4cbe55ea7d52f3b0f0589dc39fcb3a901c
vxd7 <ykonpaku13@gmail.com>
2019-09-12 20:41:23 +0300

    Fixed bug with C letter in cargo_carriage

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 require_relative 'carriage'
 
 class CargoCarriage < Carriage
-  def initialize
-    super('Cargo')
+  def initialize(id)
+    super('cargo', id)
   end
 end
