ac9391b753e45fee8206274d5c131d8e275bc449
vxd7 <ykonpaku13@gmail.com>
2019-09-21 16:00:23 +0300

    Added raise statements to Route class

--- route.rb
+++ route.rb
@@ -17,13 +17,22 @@ class Route
   end
 
   def add_intermediate_station(station)
+    raise 'Incorrect station' if station.nil?
+
+    unless station.is_a?(Station)
+      raise 'Intermediate station to insert '\
+            'must be an instance of Station'
+    end
+
     @stations.insert(-2, station)
   end
 
   def remove_intermediate_station(station)
-    unless station == @stations[0] or station == @stations[-1]
-      @stations.delete(station)
+    if station == @statins.first || station == @stations.last
+      raise 'Cannot remove starting or ending station from a route'
     end
+
+    @stations.delete(station)
   end
 
   def list_all_stations
@@ -40,6 +49,10 @@ class Route
   private
 
   def validate!
-    raise "Name cannot be empty" if name.empty?
+    raise 'Name cannot be empty' if name.empty?
+    raise 'Incorrect starting station' if @stations.first.nil?
+    raise 'Incorrect ending station' if @stations.last.nil?
+    raise 'Starting station must be an instance of Station' unless @stations.first.is_a?(Station)
+    raise 'Ending station must be an instance of Station' unless @stations.last.is_a?(Station)
   end
 end
