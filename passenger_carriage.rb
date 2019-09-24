bd674ad5df91f9bf34b3eba1866239925e839a7e
vxd7 <ykonpaku13@gmail.com>
2019-09-24 14:58:14 +0300

    Added overall_seats and avaliable_seats attributes to the PassengerCarriage

--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
+
 require_relative 'carriage'
 
 class PassengerCarriage < Carriage
-  def initialize(id)
+  attr_reader :avaliable_seats
+
+  def initialize(id, overall_seats)
+    @overall_seats = overall_seats
+    @avaliable_seats = overall_seats
+
     super('passenger', id)
+  end
+
+  def take_seat
+    raise 'No seats avaliable' if @avaliable_seats.zero?
+
+    @avaliable_seats -= 1
+  end
+
+  def taken_seats
+    @overall_seats - @avaliable_seats
   end
 end
