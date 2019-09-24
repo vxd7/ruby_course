af9b5c371480534562c8723b57818eee6e5201de
vxd7 <ykonpaku13@gmail.com>
2019-09-24 18:22:15 +0300

    Added overall_volume and avaliable_volume
    To the cargo_carriage

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -3,7 +3,25 @@
 require_relative 'carriage'
 
 class CargoCarriage < Carriage
-  def initialize(id)
+  attr_reader :avaliable_volume
+
+  def initialize(id, overall_volume)
+    @overall_volume = overall_volume
+    @avaliable_volume = overall_volume
+
     super('cargo', id)
+  end
+
+  def fill_volume(fill_value)
+    if (@avaliable_volume - fill_value).negative?
+      raise "Cannot fill #{fill_volume} volume to the "\
+            'carriage: insufficient space!'
+    end
+
+    @avaliable_volume -= fill_value
+  end
+
+  def filled_volume
+    @overall_volume - @avaliable_volume
   end
 end
